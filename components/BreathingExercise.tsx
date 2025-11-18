e9c94703f62d5f6c77626f518b97dd83fab0a788
Pgeslin <157766841+Pgeslin@users.noreply.github.com>
2025-11-19 00:01:11 +1030

    refactor: Remove unused placeholder components
    Removes empty placeholder components for BreathingExercise, Reminder, and AIGuide that were not yet implemented and were not being used. This cleans up the codebase.

--- components/BreathingExercise.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const BreathingExercise: React.FC = () => {
-  return null;
-};
-
-export default BreathingExercise;
--- components/Reminder.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Reminder: React.FC = () => {
-  return null;
-};
-
-export default Reminder;
